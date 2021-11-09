8916c319aa0038156ed5a8c9257e9ce488f4f3b6
Algorithmic-TITAN <88941142+Algorithmic-TITAN@users.noreply.github.com>
2021-11-08 21:11:33 -0500

    Add files via upload

---
+++ Java_version_of_gradient_decent_algorithm_with_add_ons.java
@@ -0,0 +1,13 @@
+/* class name MUST BE THE SAME AS THE FILE NAME */
+/*Java is capital-sensitive*/
+import java.lang.Math; /*imports java's math library*/
+import java.util.Scanner; //imports Scanner library
+import java.util.Random; //imports random library
+import java.util.Arrays; //lets you use arrays easily
+class Java_version_of_gradient_decent_algorithm_with_add_ons {  
+  public static void main(String[] args) 
+  {
+      
+  }
+
+}
