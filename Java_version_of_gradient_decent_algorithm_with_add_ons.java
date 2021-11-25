227d030fa516333155f080d800473b1a66ae7c49
Algorithmic-TITAN <88941142+Algorithmic-TITAN@users.noreply.github.com>
2021-11-24 23:32:24 -0500

    Add files via upload

--- Java_version_of_gradient_decent_algorithm_with_add_ons.java
+++ Java_version_of_gradient_decent_algorithm_with_add_ons.java
@@ -2,6 +2,9 @@
 /*Java is capital-sensitive*/
 import java.lang.Math; /*imports java's math library*/
 import java.util.Scanner; //imports Scanner library
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Random; //imports random library
 import java.util.Arrays; //lets you use arrays easily
 import java.lang.Integer;
@@ -215,37 +218,226 @@ class Java_version_of_gradient_decent_algorithm_with_add_ons {
 
 
 
+  public static double[][] find_which_way_to_nudge_values(int[] layers, int DATA_INSTANCE, String activation_functions, double[] full_population_weights, double[] full_population_biases, double[] empty_derivative_list_weights, double[] empty_derivative_lists_biases, double[][][] training_data, double[] last_layer_to_cost_effects_empty, double[] weight_surrounding_layer_numbers_empty)
+  {
+    activation_functions=activation_functions.replace("binary_step","");
+    activation_functions=activation_functions.replace("binarystep", "");
+    double[][][][] run_network_pagacked_outputs=run_network(full_population_weights, full_population_biases, training_data[DATA_INSTANCE][0], layers, activation_functions);
+    double[][][] weights_sorted_in_layers_then_second_connection_in_layer_then_first_connection_in_layer=run_network_pagacked_outputs[2];
+    double[] outputs=run_network_pagacked_outputs[0][0][0];
+    double[] node_firing_numbers=run_network_pagacked_outputs[1][0][0];
+
+    double[] last_layer_to_cost_effects=last_layer_to_cost_effects_empty;
+    for(int i=0; i<training_data[0][1].length; i++)
+    {
+      double current_node_derivative_appending_to_originoal_last_layer_to_cost_effects=2*(outputs[i]-training_data[DATA_INSTANCE][1][i]);
+      if (activation_functions.contains("sigmoid"))
+      {
+        current_node_derivative_appending_to_originoal_last_layer_to_cost_effects*=(1/(1+Math.pow(2.71828, -outputs[i])))*(1-(1/(1+Math.pow(2.71828, -outputs[i]))));
+      }
+      if ((activation_functions.contains("binary_step")) || activation_functions.contains("binarystep"))
+      {
+        current_node_derivative_appending_to_originoal_last_layer_to_cost_effects*=2*((outputs[i]-training_data[DATA_INSTANCE][1][i])*100);
+      }
+      if (activation_functions.contains("tanh") || activation_functions.contains("hyperbolic_tangent") || activation_functions.contains("hyperbolictangent"))
+      {
+        current_node_derivative_appending_to_originoal_last_layer_to_cost_effects*=1-Math.pow(((Math.pow(2.71828, outputs[i]))-(Math.pow(2.71828, -1*outputs[i])))/((Math.pow(2.71828, outputs[i]))+(Math.pow(2.71828, -1*outputs[i]))), 2);
+      }
+      if (activation_functions.contains("softplus"))
+      { 
+        current_node_derivative_appending_to_originoal_last_layer_to_cost_effects*=1/(1+(Math.pow(2.71828, -1*outputs[i])));
+      }
+      if (activation_functions.contains("gaussian") || activation_functions.contains("gaussian_function") || activation_functions.contains("gaussianfunction"))
+      {
+        current_node_derivative_appending_to_originoal_last_layer_to_cost_effects*=-2*outputs[i]*Math.pow((Math.pow(2.71828, -outputs[i])), 2);
+      }
+      last_layer_to_cost_effects[i]=current_node_derivative_appending_to_originoal_last_layer_to_cost_effects;
+    }
+    int last_layer_to_cost_effects_LEN=layers[layers.length-1];
+    int biases_went_back_counter=0;
+    double influence_amount=0; //making it avalible for the whole funcion... widening scope of var
+    double summed_influence_on_next_layer=0; //making it avalible for the whole funcion... widening scope of var
+    double effect_of_bias=0; //making it avalible for the whole funcion... widening scope of var
+    for(int i1=0; i1<layers.length-1; i1++)
+    {
+      for (int i2=0; i2<layers[layers.length-i1-1]; i2++)
+      {
+        if (i1==0)
+        {
+          influence_amount=-1;
+        }
+        else
+        {
+          influence_amount=layers[layers.length-i1];
+        }
+        summed_influence_on_next_layer=0;
+        if (!(influence_amount==-1))
+        {
+          last_layer_to_cost_effects[i2]=(double)1; //MAKE SURE YOU MAKE THIS ACTUALLY CALCULATE!!!
+        }
+        else
+        {
+          effect_of_bias=last_layer_to_cost_effects[i2];
+          last_layer_to_cost_effects[i1]=effect_of_bias;
+          biases_went_back_counter+=1;
+        }
+      if (!(influence_amount==-1))
+      {
+      last_layer_to_cost_effects_LEN=layers[layers.length-i1-1];
+      }
+      }
+      for(int i3=0; i3<(last_layer_to_cost_effects.length-last_layer_to_cost_effects_LEN); i3++)
+      {
+        last_layer_to_cost_effects[i3+last_layer_to_cost_effects_LEN]=(double)0;
+      }
+      // CURRENTLY YOU ARE THOUGH LINE 224 OF THE PYTHON CODE (IN VISUAL STUDIO). EVERYTHING WORKS.
+    }
+
+    //THE CODE BELOW IS JUST SO I CAN EASILY TEST, IT CHANGES NOTHING. DO NOT KEEP.
+    double[][] packaged_outputs={{},{}};
+    for (int asdffdsaasdf=0; asdffdsaasdf<full_population_weights.length; asdffdsaasdf++)
+    {
+      packaged_outputs[0]=Arrays.copyOf(packaged_outputs[0], packaged_outputs[0].length+1);
+      packaged_outputs[0][packaged_outputs[0].length-1]=(double)0.0;
+    }
+    for (int fdsaasdf=0; fdsaasdf<full_population_biases.length; fdsaasdf++)
+    {
+      packaged_outputs[1]=Arrays.copyOf(packaged_outputs[1], packaged_outputs[1].length+1);
+      packaged_outputs[1][packaged_outputs[1].length-1]=(double)0.0;
+    }
+    return packaged_outputs;
+  }
+
+
+
+  public static double[][] take_gradient_decent_step(int[] layers, String activation_functions, double[][][] training_data, double weights_amount, double biases_amount, double[] full_population_weights, double[] full_population_biases, double learning_rate, double[] empty_derivative_list_weights, double[] empty_derivative_lists_biases, double[] last_layer_to_cost_effects_empty, double[] weight_surrounding_layer_numbers_empty)
+  {
+    double[][] packaged_outputs={full_population_weights,full_population_biases};
+
+    for(int i=0; i<training_data.length; i++)
+    {
+      double[][] weights_and_biases_packaged_derivatives=find_which_way_to_nudge_values(layers, i, activation_functions, full_population_weights, full_population_biases, empty_derivative_list_weights, empty_derivative_lists_biases, training_data, last_layer_to_cost_effects_empty, weight_surrounding_layer_numbers_empty);
+      double[] derivatives_in_network_weights=weights_and_biases_packaged_derivatives[0];
+      double[] derivatives_in_network_biases=weights_and_biases_packaged_derivatives[1];
+
+      //CURRENTLY IT DOES THE BACKPROPOGATION FOR EVERY SINGLE TRAINING DATA PEICE. MAKE IT SO IT ONLY HAS TO FIND IT ONCE, THEN MULTIPLY BY THE DERIVATIVE AT THE END OF THE NETWORK. REMEMBER. "DATA INSTANCE" SHOULD NOT BE A VARIABLE.
+
+      for (int i1=0; i1<weights_amount; i1++)
+      {
+        packaged_outputs[0][i1]+=derivatives_in_network_weights[i1]*learning_rate*-1/training_data.length;
+      }
+      for(int i2=0; i2<biases_amount; i2++)
+      {
+        packaged_outputs[1][i2]+=derivatives_in_network_biases[i2]*learning_rate*-1/training_data.length;
+      }
+    }
+
+
+
+    return packaged_outputs;
+  }
+
+
+
+  public static double[][] empty_deriavative_lists_for_speed(double weights_amount, double biases_amount)
+  {
+    double[] empty_weights_derivative_list={};
+    double[] empty_biases_derivative_list={};
+    for (int i=0; i<weights_amount; i++)
+    {
+      empty_weights_derivative_list=Arrays.copyOf(empty_weights_derivative_list, empty_weights_derivative_list.length+1);
+      empty_weights_derivative_list[empty_weights_derivative_list.length-1]=(double) 0;
+    }
+    for (int i1=0; i1<biases_amount; i1++)
+    {
+      empty_biases_derivative_list=Arrays.copyOf(empty_biases_derivative_list, empty_biases_derivative_list.length+1);
+      empty_biases_derivative_list[empty_biases_derivative_list.length-1]=(double) 0;
+    }
+    double[][] function_outupts={empty_weights_derivative_list, empty_biases_derivative_list};
+    return function_outupts;
+  }
+
+  public static double[][] last_layer_to_cost_effects_and_weight_surrounding_layer_numbers_empty_lists_for_efficiency(int[] layers_for_efficiency)
+  {
+    double[] last_layer_to_cost_effects_EMPTY={};
+    double[] weight_surrounding_layer_numbers_EMPTY={0,0};
+    int max_layer_sums_ID_of_first_layer=-1;
+    int max_sum=0;
+    for(int i1=0; i1<layers_for_efficiency.length-1; i1++)
+    {
+      if((layers_for_efficiency[i1]+layers_for_efficiency[i1+1])>max_sum)
+      {
+        max_layer_sums_ID_of_first_layer=i1;
+        max_sum=(layers_for_efficiency[i1]+layers_for_efficiency[i1+1]);
+      }
+    }
+    for(int i=0; i<max_sum; i++)
+    {
+      last_layer_to_cost_effects_EMPTY=Arrays.copyOf(last_layer_to_cost_effects_EMPTY, last_layer_to_cost_effects_EMPTY.length+1);
+      last_layer_to_cost_effects_EMPTY[last_layer_to_cost_effects_EMPTY.length-1]=(double)0;
+    }
+    int[] layers=layers_for_efficiency;
+    double[][] function_outputs={last_layer_to_cost_effects_EMPTY, weight_surrounding_layer_numbers_EMPTY};
+    System.out.println(Arrays.toString(last_layer_to_cost_effects_EMPTY));
+    return function_outputs;
+  }
+
+
+
   public static void main(String[] args) 
   {
-    int[] LAYERS_BEING_USED={1,2,5};
+    //things you can specify
+    int[] LAYERS_BEING_USED_FOR_EFFICIENCY={1,2,7,10,5,1};
+    int[] LAYERS_BEING_USED={1,2,7,10,5,1};
     double[] INITIALIZING_RANGE={-3,3};
-    double[][][] data={{{5},{1,2,3,4,5}},{{6},{2,3,4,5,6}}};
-    double[][] all_outputs_of_init=init(LAYERS_BEING_USED, INITIALIZING_RANGE);
+    double[][][] data={{{5},{1}},{{6},{2}}, {{7},{3}}};
+    String activation_functions="";
     double[] ratios_of_data={0,1};
+    double learning_rate=0.001;
+    int epoch_amount=10; //1000000000 is the max factor of 10 becaue otherwise it would be a long or another datatype, but this is for all practical uses infinity.
 
+    //getting ready for the main computation
+    double[][] all_outputs_of_init=init(LAYERS_BEING_USED, INITIALIZING_RANGE);
+    double[][][][] train_test_data_split_outputs=train_test_data_split(data, ratios_of_data);
+    double[][][] training_data=train_test_data_split_outputs[0];
+    double[][][] testing_data=train_test_data_split_outputs[1];
     double[] full_population_weights=all_outputs_of_init[0];
     double[] full_population_biases=all_outputs_of_init[1];
     double weights_amount=all_outputs_of_init[2][0];
     double biases_amount=all_outputs_of_init[2][1];
     double[] nodes_counted_in_each_layer=all_outputs_of_init[3];
+    double[][] empty_derivative_lists=empty_deriavative_lists_for_speed(weights_amount, biases_amount);
+    double[] empty_derivative_list_weights=empty_derivative_lists[0];
+    double[] empty_derivative_lists_biases=empty_derivative_lists[1];
+    double[][] packaged_last_layer_to_cost_effects_and_weight_surrounding_layer_numbers_empty_lists_for_efficiency_outputs=last_layer_to_cost_effects_and_weight_surrounding_layer_numbers_empty_lists_for_efficiency(LAYERS_BEING_USED_FOR_EFFICIENCY);
+    double[] last_layer_to_cost_effects_EMPTY=packaged_last_layer_to_cost_effects_and_weight_surrounding_layer_numbers_empty_lists_for_efficiency_outputs[0];
+    double[] weight_surrounding_layer_numbers_EMPTY=packaged_last_layer_to_cost_effects_and_weight_surrounding_layer_numbers_empty_lists_for_efficiency_outputs[1];
 
+
+    //just running the network once while I'm creating the algorithm
     double[] inputs={2.25};
-    String activation_functions="";
     double[][][][] run_network_pagacked_outputs=run_network(full_population_weights, full_population_biases, inputs, LAYERS_BEING_USED, activation_functions);
     double[][][] weights_sorted_in_layers_then_second_connection_in_layer_then_first_connection_in_layer=run_network_pagacked_outputs[2];
     double[] outputs=run_network_pagacked_outputs[0][0][0];
     double[] node_firing_numbers=run_network_pagacked_outputs[1][0][0];
-    System.out.println(Arrays.toString(outputs));
     //If you get an NaN as an output, just make the initializing range smaller.
-    //Remember to code the training testing data split function, and that you've already coded the find_cost and test functions.    double[] ratios_of_data={0, 1}; //this is only an approximation... YOU CAN DO 0,1!
+    //Remember to code the training testing data split function, and that you've already coded the find_cost and test functions.
+    //MAKE SURE THE ACTIVATION FUNCTIONS' DERIVATIVES WORK.
 
+    //Main loop that actually trains the network
+    for (int i=0; i<epoch_amount; i++)
+    {
+    double[][] new_weights_and_biases=take_gradient_decent_step(LAYERS_BEING_USED, activation_functions, training_data, weights_amount, biases_amount, full_population_weights, full_population_biases, learning_rate, empty_derivative_list_weights, empty_derivative_lists_biases, last_layer_to_cost_effects_EMPTY, weight_surrounding_layer_numbers_EMPTY);
+    full_population_weights=new_weights_and_biases[0];
+    full_population_biases=new_weights_and_biases[1];
+    }
+    //REMEMBER THAT SINCE JAVA IS SLOWER THAN PYTHON AT LIST APPENDING, MINIMALIZE APPENDING TO MAKE IT FASTER (YOU CAN DO THIS BY ONLY CHANGING PARTS OF A LIST, INSTEAD OF RECREATING THEM) DO THIS FOR EVERY FUNCTION, INCLUDING ONES ALREADY MADE.
 
+    System.out.println("Stuff done");
 
-    double[][][][] train_test_data_split_outputs=train_test_data_split(data, ratios_of_data);
-    double[][][] training_data=train_test_data_split_outputs[0];
-    double[][][] testing_data=train_test_data_split_outputs[1];
-  }
+  } 
 
 }
+
 
 //Remember to go into https://github.com/Algorithmic-TITAN/Java_version_of_gradient_decent_algorithm_with_add_ons and drag and drop the file from file explorer (in folder src) into the repository to update the git repository.
